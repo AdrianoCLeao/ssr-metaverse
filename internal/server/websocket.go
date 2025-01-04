c315a5a372d4e49f5d009eddf0eea66ca04a37da
AdrianoCLeao <adrianocoelho133@gmail.com>
2025-01-03 21:28:25 -0300

    websockets: using procedural renderization on the HandleWebSocket method

--- internal/server/websocket.go
+++ internal/server/websocket.go
@@ -24,11 +24,21 @@ type Server struct {
 }
 
 func isWithinDistance(clientPos, objectPos [3]float64, maxDistance float64) bool {
-    dx := clientPos[0] - objectPos[0]
-    dy := clientPos[1] - objectPos[1]
-    dz := clientPos[2] - objectPos[2]
-    distance := math.Sqrt(dx*dx + dy*dy + dz*dz)
-    return distance <= maxDistance
+	dx := clientPos[0] - objectPos[0]
+	dy := clientPos[1] - objectPos[1]
+	dz := clientPos[2] - objectPos[2]
+	distance := math.Sqrt(dx*dx + dy*dy + dz*dz)
+	return distance <= maxDistance
+}
+
+func filterObjects(world *World, clientPos [3]float64, maxDistance float64) map[string]Object {
+	filtered := make(map[string]Object)
+	for id, object := range world.Objects {
+		if isWithinDistance(clientPos, object.Position, maxDistance) {
+			filtered[id] = object
+		}
+	}
+	return filtered
 }
 
 func NewServer() *Server {
@@ -46,7 +56,7 @@ func (s *Server) Start(addr string) error {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"}, 
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
@@ -66,84 +76,74 @@ func (s *Server) Start(addr string) error {
 	return router.Run(addr)
 }
 
-func filterObjects(world *World, clientPos [3]float64, maxDistance float64) map[string]Object {
-    filtered := make(map[string]Object)
-    for id, object := range world.Objects {
-        if isWithinDistance(clientPos, object.Position, maxDistance) {
-            filtered[id] = object
-        }
-    }
-    return filtered
-}
-
 func (s *Server) HandleWebSocket(w http.ResponseWriter, r *http.Request) {
-    conn, err := s.Upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        fmt.Println("Error upgrading connection:", err)
-        return
-    }
-    defer conn.Close()
+	conn, err := s.Upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error upgrading connection:", err)
+		return
+	}
+	defer conn.Close()
 
-    client := &Client{
-        ID:   r.RemoteAddr,
-        Conn: conn,
-    }
+	client := &Client{
+		ID:   r.RemoteAddr,
+		Conn: conn,
+	}
 
-    s.Mutex.Lock()
-    s.Clients[client.ID] = client
-    s.Mutex.Unlock()
+	s.Mutex.Lock()
+	s.Clients[client.ID] = client
+	s.Mutex.Unlock()
 
-    defer func() {
-        s.Mutex.Lock()
-        delete(s.Clients, client.ID)
-        s.Mutex.Unlock()
-    }()
+	defer func() {
+		s.Mutex.Lock()
+		delete(s.Clients, client.ID)
+		s.Mutex.Unlock()
+	}()
 
-    world := NewWorld()
-    //clientPos := [3]float64{0, 0, 0}
-    
-    prevFilteredObjects := make(map[string]Object)
+	world := NewWorld()
 
-    for {
-        _, msg, err := conn.ReadMessage()
-        if err != nil {
-            fmt.Println("Error reading message:", err)
-            break
-        }
-    
-        var payload struct {
-            Position [3]float64 `json:"position"`
-            Rotation [3]float64 `json:"rotation"`
-        }
-        if err := json.Unmarshal(msg, &payload); err != nil {
-            fmt.Println("Error unmarshalling message:", err)
-            continue
-        }
-    
-        filteredObjects := world.Objects
-    
-        changes := map[string]interface{}{
-            "added":   make(map[string]Object),
-            "removed": make([]string, 0),
-        }
-    
-        for id, object := range filteredObjects {
-            if _, exists := prevFilteredObjects[id]; !exists {
-                changes["added"].(map[string]Object)[id] = object
-            }
-        }
-        for id := range prevFilteredObjects {
-            if _, exists := filteredObjects[id]; !exists {
-                changes["removed"] = append(changes["removed"].([]string), id)
-            }
-        }
-    
-        prevFilteredObjects = filteredObjects
-    
-        err = conn.WriteJSON(changes)
-        if err != nil {
-            fmt.Println("Error writing JSON:", err)
-            break
-        }
-    }    
+	prevFilteredObjects := make(map[string]Object)
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Error reading message:", err)
+			break
+		}
+
+		var payload struct {
+			Position [3]float64 `json:"position"`
+			Rotation [3]float64 `json:"rotation"`
+		}
+		if err := json.Unmarshal(msg, &payload); err != nil {
+			fmt.Println("Error unmarshalling message:", err)
+			continue
+		}
+
+		maxDistance := 50.0
+		filteredObjects := filterObjects(world, payload.Position, maxDistance)
+
+		changes := map[string]interface{}{
+			"added":   make(map[string]Object),
+			"removed": make([]string, 0),
+		}
+
+		for id, object := range filteredObjects {
+			if _, exists := prevFilteredObjects[id]; !exists {
+				changes["added"].(map[string]Object)[id] = object
+			}
+		}
+
+		for id := range prevFilteredObjects {
+			if _, exists := filteredObjects[id]; !exists {
+				changes["removed"] = append(changes["removed"].([]string), id)
+			}
+		}
+
+		prevFilteredObjects = filteredObjects
+
+		if err = conn.WriteJSON(changes); err != nil {
+			fmt.Println("Error writing JSON:", err)
+			break
+		}
+	}
 }
